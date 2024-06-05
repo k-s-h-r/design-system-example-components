48e3c8d6a31f07fff70694da814fa9e20de71b51
k-s-h-r <kasahara.mail@gmail.com>
2024-06-05 22:46:57 +0900

    Fix tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,25 +5,11 @@ export default {
   // darkMode: process.env.NODE_ENV === 'production' ? undefined : ['selector', '[data-mode="dark"]'],
   // 仮：上記対応はstorybookのダークモード切り替えが効かなくなるため一旦保留
   darkMode: ['selector', '[data-mode="dark"]'],
-  heme: {
+  theme: {
     extend: {
       fontFamily: {
         mono: ['Noto Sans Mono'],
       },
-    },
-    keyframes: {
-      'accordion-down': {
-        from: { height: '0' },
-        to: { height: 'var(--radix-accordion-content-height)' },
-      },
-      'accordion-up': {
-        from: { height: 'var(--radix-accordion-content-height)' },
-        to: { height: '0' },
-      },
-    },
-    animation: {
-      'accordion-down': 'accordion-down 0.2s ease-out',
-      'accordion-up': 'accordion-up 0.2s ease-out',
     },
   },
   plugins: [
